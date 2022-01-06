e66a5b703fc1c580794741163e9a9397e4b1d506
PORTB <PORTB@noreply.codeberg.org>
2022-01-06 20:22:42 +1100

    Fix config comment

--- src/main/java/portb/biggerstacks/config/DefaultServerConfig.java
+++ src/main/java/portb/biggerstacks/config/DefaultServerConfig.java
@@ -11,7 +11,7 @@ public class DefaultServerConfig
     public static final ForgeConfigSpec.IntValue maxStackCount;
 
     static{
-        builder.comment("This file serves as the default config for new worlds/(server) when they are created");
+        builder.comment("This file serves as the default config for new worlds when they are created");
         maxStackCount = builder.comment("Maximum stack size for items. Items that are able to stack more than 1 item " +
                 "(i.e. swords, tools, etc) are not effected, stack size for everything else is raised to this value.", " While you *can* go up to " + Integer.MAX_VALUE +
                 ", that doesn't mean you *should*").defineInRange("Max stack size", 999, 1, Integer.MAX_VALUE);
