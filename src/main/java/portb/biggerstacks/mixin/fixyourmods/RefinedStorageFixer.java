3c2f52df4a3d25690021ae310a2a19dbe5d49bc8
PORTB <PORTB@noreply.codeberg.org>
2022-01-07 13:27:50 +1100

    Add RS compat

---
+++ src/main/java/portb/biggerstacks/mixin/fixyourmods/RefinedStorageFixer.java
@@ -0,0 +1,18 @@
+package portb.biggerstacks.mixin.fixyourmods;
+
+import com.refinedmods.refinedstorage.apiimpl.network.grid.handler.ItemGridHandler;
+import org.checkerframework.common.value.qual.IntVal;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(ItemGridHandler.class)
+public class RefinedStorageFixer
+{
+    @ModifyConstant(method = "onExtract(Lnet/minecraft/server/level/ServerPlayer;Ljava/util/UUID;II)V", constant = @Constant(intValue = 64), remap = false, require = 0)
+    private int biggerStackExtract(int value)
+    {
+        return ServerConfig.maxStackCount.get();
+    }
+}
