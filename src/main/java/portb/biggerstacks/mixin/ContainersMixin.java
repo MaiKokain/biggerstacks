fdeccd05d441794f3b9e4552a58dbb9643d02da8
PORTB <PORTB@noreply.codeberg.org>
2022-01-07 22:37:12 +1100

    Scale how much a stack is split up when dropped from a broken chest based on max stack size

---
+++ src/main/java/portb/biggerstacks/mixin/ContainersMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin;
+
+import net.minecraft.world.Containers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(Containers.class)
+public class ContainersMixin
+{
+    @ModifyConstant(method = "dropItemStack", constant = {@Constant(intValue = 21), @Constant(intValue = 10)})
+    private static int scaleDroppedItemStackSize(int value)
+    {
+        return value * ServerConfig.INSTANCE.maxStackCount.get() / 64;
+    }
+}
