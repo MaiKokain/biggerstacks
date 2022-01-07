b711f3d17956607291b3071a4cf9fbfa0186c93b
PORTB <PORTB@noreply.codeberg.org>
2022-01-08 01:16:09 +1100

    Add config option for fat stacks

--- src/main/java/portb/biggerstacks/mixin/client/ItemEntityRendererMixin.java
+++ src/main/java/portb/biggerstacks/mixin/client/ItemEntityRendererMixin.java
@@ -12,42 +12,48 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import portb.biggerstacks.FastLog2;
+import portb.biggerstacks.config.ClientConfig;
+import portb.biggerstacks.config.ServerConfig;
 
 @Mixin(ItemEntityRenderer.class)
 public class ItemEntityRendererMixin
 {
     //make stacks of items dropped on the ground look extra thicc
-//    @Inject(method = "getRenderAmount", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getRenderAmount", at = @At("HEAD"), cancellable = true)
     private void getBiggerStackRenderAmount(ItemStack stack, CallbackInfoReturnable<Integer> returnInfo)
     {
-        returnInfo.cancel();
-        returnInfo.setReturnValue(calculateDisplayStackAmount(stack.getCount()));
+        if(ClientConfig.enableFatStacks.get())
+        {
+            returnInfo.cancel();
+            returnInfo.setReturnValue(calculateDisplayStackAmount(stack.getCount()));
+        }
     }
 
     private int calculateDisplayStackAmount(int count)
     {
-        return (FastLog2.fastLog2(count) / 2) + 1;
+        return Math.max(1, FastLog2.fastLog2(count) / 2);
     }
 
-    /*@ModifyArg(method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", ordinal = 4), index = 2)*/
     @ModifyConstant(method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             constant = @Constant(doubleValue = 0.09375F))
     private double reduceItemSpread(double value, ItemEntity entity, float p_115037_, float p_115038_, PoseStack p_115039_, MultiBufferSource p_115040_, int p_115041_)
     {
-        int displayCount = calculateDisplayStackAmount(entity.getItem().getCount());
-        double distanceScale = displayCount < 5 ? value : value * ((double) 5 / displayCount);
+        if(!ClientConfig.enableFatStacks.get())
+            return value;
 
-        return distanceScale;
+        int displayCount = calculateDisplayStackAmount(entity.getItem().getCount());
+        return displayCount < 5 ? value : value * ((double) 5 / displayCount);
     }
 
     @ModifyConstant(method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             constant = @Constant(floatValue = -0.09375F))
     private float reduceItemSpreadFloat(float value, ItemEntity entity, float p_115037_, float p_115038_, PoseStack p_115039_, MultiBufferSource p_115040_, int p_115041_)
     {
-        int displayCount = calculateDisplayStackAmount(entity.getItem().getCount());
-        float distanceScale = displayCount < 5 ? value : value * ((float) 5 / displayCount);
+        if(!ClientConfig.enableFatStacks.get())
+            return value;
 
-        return distanceScale;
+        int displayCount = calculateDisplayStackAmount(entity.getItem().getCount());
+        
+        return displayCount < 5 ? value : value * ((float) 5 / displayCount);
     }
 }
