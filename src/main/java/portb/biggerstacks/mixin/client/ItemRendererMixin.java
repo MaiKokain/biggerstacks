785540eb5c789d7a95c92a0ac3923e8b1e8ea5a7
PORTB <alexander.pandaman@gmail.com>
2022-01-07 15:30:16 +1100

    Fix number shortening It was not showing the decimal like I wanted it to

--- src/main/java/portb/biggerstacks/mixin/client/ItemRendererMixin.java
+++ src/main/java/portb/biggerstacks/mixin/client/ItemRendererMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.item.ItemStack;
+import org.lwjgl.system.CallbackI;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import portb.biggerstacks.config.ClientConfig;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.text.DecimalFormat;
 
 import static portb.biggerstacks.Constants.*;
@@ -22,20 +25,24 @@ import static portb.biggerstacks.Constants.*;
 public class ItemRendererMixin
 {
     private static final DecimalFormat BILLION_FORMAT = new DecimalFormat("#.##B");
-    private static final DecimalFormat MILLION_FORMAT = new DecimalFormat("#.#M");
-    private static final DecimalFormat THOUSAND_FORMAT = new DecimalFormat("#.#K");
+    private static final DecimalFormat MILLION_FORMAT = new DecimalFormat("#.##M");
+    private static final DecimalFormat THOUSAND_FORMAT = new DecimalFormat("#.##K");
 
     @Redirect(method = "renderGuiItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Ljava/lang/String;valueOf(I)Ljava/lang/String;"))
     private String getStringForBigStackCount(int count)
     {
         if (ClientConfig.enableNumberShortening.get())
         {
-            if (count > ONE_BILLION)
-                return BILLION_FORMAT.format(Math.round((double) count / ONE_BILLION));
-            else if (count > ONE_MILLION)
-                return MILLION_FORMAT.format(Math.round((double) count / ONE_MILLION));
-            else if (count > ONE_THOUSAND)
-                return THOUSAND_FORMAT.format(Math.round((double) count / ONE_THOUSAND));
+            BigDecimal decimal = new BigDecimal(count).round(new MathContext(4)); //pinnacle of over engineering
+
+            var value = decimal.doubleValue();
+
+            if (value >= ONE_BILLION)
+                return BILLION_FORMAT.format(value  / ONE_BILLION);
+            else if (value >= ONE_MILLION)
+                return MILLION_FORMAT.format(value / ONE_MILLION);
+            else if (value > ONE_THOUSAND)
+                return THOUSAND_FORMAT.format(value / ONE_THOUSAND);
         }
 
         return String.valueOf(count);
