69a2dfeea24589a4d0831a5162635822124ea27f
PORTB <PORTB@noreply.codeberg.org>
2022-01-08 15:16:02 +1100

    Fix inserting into IE containers

---
+++ src/main/java/portb/biggerstacks/mixin/compat/immersiveengineering/IEInventoryHandlerMixin.java
@@ -0,0 +1,25 @@
+package portb.biggerstacks.mixin.compat.immersiveengineering;
+
+import blusunrize.immersiveengineering.common.util.inventory.IEInventoryHandler;
+import blusunrize.immersiveengineering.common.util.inventory.IIEInventory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(IEInventoryHandler.class)
+public class IEInventoryHandlerMixin
+{
+    @Redirect(method = "insertItem", at = @At(value = "INVOKE", target = "Lblusunrize/immersiveengineering/common/util/inventory/IIEInventory;getSlotLimit(I)I"), remap = false, require = 0)
+    private int fixSlotLimit(IIEInventory inventory, int slot)
+    {
+        var limit = inventory.getSlotLimit(slot);
+
+        if(limit == 64)
+        {
+            return ServerConfig.INSTANCE.maxStackCount.get();
+        }
+
+        return limit;
+    }
+}
