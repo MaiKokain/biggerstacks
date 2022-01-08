e77cafdb57277f851bd3155c3f50ddaa7accc549
PORTB <alexander.pandaman@gmail.com>
2022-01-08 15:52:15 +1100

    Compat for RS exporter, importer, interface, etc

---
+++ src/main/java/portb/biggerstacks/mixin/compat/refinedstorage/UpgradeItemHandlerMixin.java
@@ -0,0 +1,22 @@
+package portb.biggerstacks.mixin.compat.refinedstorage;
+
+import com.refinedmods.refinedstorage.inventory.item.UpgradeItemHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(UpgradeItemHandler.class)
+public class UpgradeItemHandlerMixin
+{
+    @Inject(method = "getStackInteractCount", at=@At("RETURN"),cancellable = true, require = 0, remap = false)
+    private void fixStackInteractCount(CallbackInfoReturnable<Integer> returnInfo)
+    {
+        if(returnInfo.getReturnValue() == 64)
+        {
+            returnInfo.cancel();
+            returnInfo.setReturnValue(ServerConfig.INSTANCE.maxStackCount.get());
+        }
+    }
+}
