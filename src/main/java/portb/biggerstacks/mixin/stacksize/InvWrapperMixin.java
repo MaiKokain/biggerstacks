2e501d20d1bcdf0784c0e4931e8a720199e3098b
PORTB <alexander.pandaman@gmail.com>
2022-01-08 15:17:17 +1100

    Add another vanilla mixin

---
+++ src/main/java/portb/biggerstacks/mixin/stacksize/InvWrapperMixin.java
@@ -0,0 +1,24 @@
+package portb.biggerstacks.mixin.stacksize;
+
+import net.minecraftforge.items.wrapper.InvWrapper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(InvWrapper.class)
+public class InvWrapperMixin
+{
+    @Redirect(method = "insertItem", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/items/wrapper/InvWrapper;getSlotLimit(I)I"), remap = false)
+    private int fixMaxStackSize(InvWrapper instance, int slot)
+    {
+        var value = instance.getSlotLimit(slot);
+
+        if(value == 64)
+        {
+            return ServerConfig.INSTANCE.maxStackCount.get();
+        }
+
+        return value;
+    }
+}
