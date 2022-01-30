79dc31d1d0d9f0d8510077dac5746382e564aa28
PORTB <PORTB@noreply.codeberg.org>
2022-01-31 09:54:01 +1100

    Add colossal chests compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/colossalchests/BlockEntityColossalChestMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin.compat.colossalchests;
+
+import org.cyclops.colossalchests.blockentity.BlockEntityColossalChest;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(BlockEntityColossalChest.class)
+public class BlockEntityColossalChestMixin
+{
+    @ModifyConstant(method = {"constructInventory", "constructInventoryDebug"}, constant = @Constant(intValue = 64), remap = false)
+    private int increaseStackSize(int value)
+    {
+        return ServerConfig.INSTANCE.maxStackCount.get();
+    }
+}
---
+++ src/main/java/portb/biggerstacks/mixin/compat/colossalchests/BlockEntityUncolossalChestMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin.compat.colossalchests;
+
+import org.cyclops.colossalchests.blockentity.BlockEntityUncolossalChest;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(BlockEntityUncolossalChest.class)
+public class BlockEntityUncolossalChestMixin
+{
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 64), remap = false)
+    private int increaseStackSize(int val)
+    {
+        return ServerConfig.INSTANCE.maxStackCount.get();
+    }
+}
