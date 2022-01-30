92fdab0f2890f9e5f81927d272d061e441e5a855
PORTB <alexander.pandaman@gmail.com>
2022-01-31 09:54:01 +1100

    Add colossal chests compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/colossalchests/ContainerColossalChestMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin.compat.colossalchests;
+
+import org.cyclops.colossalchests.inventory.container.ContainerColossalChest;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+
+@Mixin(ContainerColossalChest.class)
+public class ContainerColossalChestMixin
+{
+    @ModifyConstant(method = "<init>(ILnet/minecraft/world/entity/player/Inventory;Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 64))
+    private static int increaseStackSize(int value)
+    {
+        return ServerConfig.INSTANCE.maxStackCount.get();
+    }
+}
