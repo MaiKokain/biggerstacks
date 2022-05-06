ee0d5a6d8f78d66a526b7a804c90b97fcc5a8e3e
PORTB <alexander.pandaman@gmail.com>
2022-05-06 19:51:06 +1000

    Add compat for PlayerTabs

---
+++ src/main/java/portb/biggerstacks/mixin/compat/playertabs/TabsCapabilityMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin.compat.playertabs;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.ServerConfig;
+import wolforce.playertabs.TabsCapability;
+
+@Mixin(TabsCapability.class)
+public class TabsCapabilityMixin
+{
+    @ModifyConstant(require = 0, method = {"saveInvToCurrTab", "writeTabToInv"}, constant = @Constant(intValue = 64), remap = false)
+    private int increaseMaxStackSize(int constant)
+    {
+        return ServerConfig.INSTANCE.maxStackCount.get();
+    }
+}
