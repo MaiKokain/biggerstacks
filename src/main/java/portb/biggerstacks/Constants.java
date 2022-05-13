572a116ce4cefb42714984de6eee0c8e62999884
PORTB <PORTB@noreply.codeberg.org>
2022-05-14 09:10:31 +1000

    Add blacklist tag

--- src/main/java/portb/biggerstacks/Constants.java
+++ src/main/java/portb/biggerstacks/Constants.java
@@ -1,8 +1,17 @@
 package portb.biggerstacks;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+import java.text.DecimalFormat;
+
 public class Constants
 {
     public static final int ONE_BILLION = 1_000_000_000;
     public static final int ONE_MILLION = 1_000_000;
     public static final int ONE_THOUSAND = 1_000;
+
+    public static final String MOD_ID = "biggerstacks";
 }
