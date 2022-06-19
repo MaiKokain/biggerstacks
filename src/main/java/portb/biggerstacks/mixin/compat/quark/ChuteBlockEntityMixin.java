bf2de21e76ff11bbb349aeb19f11b083d8f61857
PORTB <alexander.pandaman@gmail.com>
2022-06-19 20:52:51 +1000

    Add quark compat for chutes and chest boats

---
+++ src/main/java/portb/biggerstacks/mixin/compat/quark/ChuteBlockEntityMixin.java
@@ -0,0 +1,17 @@
+package portb.biggerstacks.mixin.compat.quark;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.AutoSidedConfig;
+import vazkii.quark.content.automation.block.be.ChuteBlockEntity;
+
+@Mixin(targets = "vazkii/quark/content/automation/block/be/ChuteBlockEntity$1")
+public class ChuteBlockEntityMixin
+{
+    @ModifyConstant(method = "getSlotLimit(I)I", constant = @Constant(intValue = 64), remap = false, require = 0)
+    private int increaseSlotLimit(int val)
+    {
+        return AutoSidedConfig.getMaxStackSize();
+    }
+}
