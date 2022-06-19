17a315b46ceddfc40a56b1adc042d01cda1b4d0e
PORTB <alexander.pandaman@gmail.com>
2022-06-19 13:28:33 +1000

    Add mekanism compat and bump version

---
+++ src/main/java/portb/biggerstacks/mixin/compat/mekanism/TileEntityQIOFilterHandlerMixin.java
@@ -0,0 +1,27 @@
+package portb.biggerstacks.mixin.compat.mekanism;
+
+import mekanism.common.tile.qio.TileEntityQIOFilterHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.AutoSidedConfig;
+
+@Mixin(value = TileEntityQIOFilterHandler.class, remap = false)
+public class TileEntityQIOFilterHandlerMixin
+{
+    @ModifyConstant(method = "getMaxTransitCount", constant = @Constant(intValue = 64))
+    private int increaseTransferRate(int val){
+        if(AutoSidedConfig.increaseTransferRate())
+            return AutoSidedConfig.getMaxStackSize();
+        else
+            return val;
+    }
+
+    @ModifyConstant(method = "getMaxTransitCount", constant = @Constant(intValue = 32))
+    private int increaseUpgradeTransferRate(int val){
+        if(AutoSidedConfig.increaseTransferRate())
+            return AutoSidedConfig.getMaxStackSize() / 2;
+        else
+            return val;
+    }
+}
