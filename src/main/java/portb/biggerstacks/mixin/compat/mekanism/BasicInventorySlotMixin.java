438bb0fc62806adca5c28dffd45b7bcb1616d8c0
PORTB <alexander.pandaman@gmail.com>
2022-06-19 11:46:29 +1000

    Add mekanism compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/mekanism/BasicInventorySlotMixin.java
@@ -0,0 +1,24 @@
+package portb.biggerstacks.mixin.compat.mekanism;
+
+import mekanism.common.inventory.slot.BasicInventorySlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import portb.biggerstacks.config.AutoSidedConfig;
+
+@Mixin(BasicInventorySlot.class)
+public class BasicInventorySlotMixin
+{
+    @ModifyVariable(method = "<init>(ILjava/util/function/BiPredicate;Ljava/util/function/BiPredicate;Ljava/util/function/Predicate;Lmekanism/api/IContentsListener;II)V",
+                    at = @At(value = "HEAD"),
+                    ordinal = 0,
+                    require = 0,
+                    argsOnly = true)
+    private static int increaseStackSize(int value)
+    {
+        if (value != 1)
+            return (value / 64) * AutoSidedConfig.getMaxStackSize();
+        else
+            return 1;
+    }
+}
