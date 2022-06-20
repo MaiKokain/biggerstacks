4d2c43c60ab2d77312e57a07ff746b2ddea98407
PORTB <alexander.pandaman@gmail.com>
2022-06-20 13:04:18 +1000

    Fix sophisticated backpacks overstacking without when there are no upgrades in the backpack
    The mod no longer increases the stack size for sophisticated backpacks

--- src/main/java/portb/biggerstacks/mixin/stacksize/SlotItemHandlerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/stacksize/SlotItemHandlerMixin.java
@@ -1,10 +1,13 @@
 package portb.biggerstacks.mixin.stacksize;
 
+import fuzs.configmenusforge.lib.core.ModLoaderEnvironment;
 import net.minecraftforge.items.SlotItemHandler;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageInventorySlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import portb.biggerstacks.Constants;
 import portb.biggerstacks.config.AutoSidedConfig;
 
 @Mixin(SlotItemHandler.class)
@@ -13,6 +16,11 @@ public class SlotItemHandlerMixin
     @Inject(method = "getMaxStackSize()I", at = @At("RETURN"), cancellable = true)
     private void increaseStackLimit(CallbackInfoReturnable<Integer> returnInfo)
     {
+        //Do not change slot size for sophisticated backpacks as it causes issues with overstacking.
+        //noinspection ConstantConditions
+        if(ModLoaderEnvironment.isModLoaded(Constants.SOPHISTICATED_BACKPACKS) && ((Object)this) instanceof StorageInventorySlot)
+            return;
+
         if (returnInfo.getReturnValue() == 64)
         {
             returnInfo.cancel();
