0f631262da08d403adf29e9a6f56d13d2685e8e5
PORTB <PORTB@noreply.codeberg.org>
2022-06-21 14:30:37 +1000

    Fix sophisticated backpacks compat
    Please work this time

--- src/main/java/portb/biggerstacks/mixin/ItemMixin.java
+++ src/main/java/portb/biggerstacks/mixin/ItemMixin.java
@@ -6,14 +6,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import portb.biggerstacks.BiggerStacks;
+import portb.biggerstacks.util.ItemExtension;
 import portb.biggerstacks.config.AutoSidedConfig;
 
 @Mixin(Item.class)
-public class ItemMixin
+public class ItemMixin implements ItemExtension
 {
+    private Integer originalMaxStackSize = null;
+
     @Inject(method = "getMaxStackSize", at = @At("RETURN"), cancellable = true)
     private void increaseStackLimit(CallbackInfoReturnable<Integer> returnInfo)
     {
+        //Store the original stack size of the item
+        originalMaxStackSize = returnInfo.getReturnValue();
+
         Item item = ((Item) (Object) this);
 
         try
@@ -35,5 +41,37 @@ public class ItemMixin
             returnInfo.cancel();
             returnInfo.setReturnValue(AutoSidedConfig.getMaxStackSize());
         }
+    }
+
+    @Override
+    public int getOriginalMaxStackSize()
+    {
+        if(originalMaxStackSize == null)
+            //noinspection deprecation,ResultOfMethodCallIgnored
+            ((Item)(Object)this).getMaxStackSize();
+
+        //noinspection ConstantConditions
+        return originalMaxStackSize;
+    }
+
+    @Override
+    public boolean hasStackSizeBeenIncreased() {
+        try
+        {
+            @SuppressWarnings("ConstantConditions") Item item = (Item)(Object)this;
+
+            //if whitelist is enabled and the item isn't whitelisted, don't increase its stack size
+            if (AutoSidedConfig.isUsingWhitelist() && !item.is(BiggerStacks.WHITELIST_TAG))
+                return false;
+                //check if this item has the blacklist tag, and if it does, don't increase its stack size
+            else if (item.is(BiggerStacks.BLACKLIST_TAG))
+                return false;
+        }
+        catch (IllegalStateException e)
+        {
+            System.err.println("Tags are not bound at this time! Assuming all items are whitelisted");
+        }
+
+        return true;
     }
 }
---
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/BackpackInventorySlotAccessor.java
@@ -0,0 +1,13 @@
+package portb.biggerstacks.mixin.compat.sophisticatedbackpacks;
+
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackInventoryHandler;
+import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.BackpackInventorySlot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(BackpackInventorySlot.class)
+public interface BackpackInventorySlotAccessor
+{
+    @Accessor("inventoryHandler")
+    BackpackInventoryHandler accessInventoryHandler();
+}
