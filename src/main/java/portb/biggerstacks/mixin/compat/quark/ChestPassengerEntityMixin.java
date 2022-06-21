645223de39b36b864161b9d670f667256a3b2172
PORTB <alexander.pandaman@gmail.com>
2022-06-21 17:51:37 +1000

    Fix mixin targets

--- src/main/java/portb/biggerstacks/mixin/compat/quark/ChestPassengerEntityMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/quark/ChestPassengerEntityMixin.java
@@ -1,5 +1,6 @@
 package portb.biggerstacks.mixin.compat.quark;
 
+import net.minecraft.inventory.IInventory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
@@ -7,10 +8,11 @@ import portb.biggerstacks.config.AutoSidedConfig;
 import vazkii.quark.content.management.entity.ChestPassengerEntity;
 
 @Mixin(ChestPassengerEntity.class)
-public class ChestPassengerEntityMixin
+public abstract class ChestPassengerEntityMixin
 {
-    @ModifyConstant(method = "getMaxStackSize", constant = @Constant(intValue = 64), remap = false, require = 0)
-    private int increaseStackLimit(int val){
+    @ModifyConstant(method = "getMaxStackSize", constant = @Constant(intValue = 64), require = 0)
+    private int increaseStackLimit(int val)
+    {
         return AutoSidedConfig.getMaxStackSize();
     }
 }
