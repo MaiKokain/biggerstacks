b4ffc0915c191a91bcf8aadaaab82bdeec2d2ad1
PORTB <PORTB@noreply.codeberg.org>
2022-06-22 14:48:53 +1000

    Add Tinkers compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/tinkers/PartChestIItemHandlerMixin.java
@@ -0,0 +1,19 @@
+package portb.biggerstacks.mixin.compat.tinkers;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.AutoSidedConfig;
+import slimeknights.tconstruct.tables.block.entity.chest.PartChestBlockEntity;
+
+@Mixin(PartChestBlockEntity.PartChestItemHandler.class)
+public class PartChestIItemHandlerMixin
+{
+    /**
+     * Increases stack limit for part chest to 1/8th of the maximum stack size
+     */
+    @ModifyConstant(method = "getSlotLimit", constant = @Constant(intValue = 8), remap = false)
+    private int increaseStackLimit(int val) {
+        return AutoSidedConfig.getMaxStackSize() / 8;
+    }
+}
