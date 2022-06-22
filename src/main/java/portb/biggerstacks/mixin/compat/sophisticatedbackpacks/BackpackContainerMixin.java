40349c127e54cd7c907a8cec1e8d21756d14ece1
PORTB <PORTB@noreply.codeberg.org>
2022-06-22 18:41:41 +1000

    Run code cleanup

--- src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/BackpackContainerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/BackpackContainerMixin.java
@@ -1,22 +1,17 @@
 package portb.biggerstacks.mixin.compat.sophisticatedbackpacks;
 
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
-import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageInventorySlot;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import portb.biggerstacks.config.AutoSidedConfig;
-import portb.biggerstacks.util.ItemExtension;
 
 @Mixin(StorageContainerMenuBase.class)
 public abstract class BackpackContainerMixin
 {
     @ModifyConstant(method = "calculateMaxCountForStack", constant = @Constant(intValue = 64), remap = false)
-    private static int increaseStackLimit(int val){
+    private static int increaseStackLimit(int val)
+    {
         return AutoSidedConfig.getMaxStackSize();
     }
 }
--- src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/InventorySorterMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/InventorySorterMixin.java
@@ -1,16 +1,10 @@
 package portb.biggerstacks.mixin.compat.sophisticatedbackpacks;
 
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.p3pp3rf1y.sophisticatedcore.inventory.ItemStackKey;
 import net.p3pp3rf1y.sophisticatedcore.util.InventorySorter;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import portb.biggerstacks.config.AutoSidedConfig;
-import portb.biggerstacks.util.ItemExtension;
 
 /**
  * Fixes items being overstacked when using the sort function in a sophisticated backpack
@@ -19,7 +13,8 @@ import portb.biggerstacks.util.ItemExtension;
 public class InventorySorterMixin
 {
     @ModifyConstant(method = "placeStack", constant = @Constant(intValue = 64), remap = false)
-    private static int increaseStackLimit(int val){
+    private static int increaseStackLimit(int val)
+    {
         return AutoSidedConfig.getMaxStackSize();
     }
 }
