b9991377a12ba2bfad7cbf04863e24cdbaab7b32
PORTB <PORTB@noreply.codeberg.org>
2022-06-22 16:56:33 +1000

    Add tinkers compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/tinkers/HeaterItemHandlerMixin.java
@@ -0,0 +1,19 @@
+package portb.biggerstacks.mixin.compat.tinkers;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.AutoSidedConfig;
+import slimeknights.tconstruct.smeltery.tileentity.inventory.HeaterItemHandler;
+
+@Mixin(HeaterItemHandler.class)
+public class HeaterItemHandlerMixin
+{
+    /**
+     * Increases slot size for the seared heater
+     */
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 64))
+    private static int increaseStackLimit(int val){
+        return AutoSidedConfig.getMaxStackSize();
+    }
+}
