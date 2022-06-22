e30c0bdd0ab6cf5396c2c24922e84ff5145a0589
PORTB <alexander.pandaman@gmail.com>
2022-06-22 16:58:04 +1000

    Fix item stacks on the ground that are bigger than 64 not merging

---
+++ src/main/java/portb/biggerstacks/mixin/ItemEntityMixin.java
@@ -0,0 +1,19 @@
+package portb.biggerstacks.mixin;
+
+import net.minecraft.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import portb.biggerstacks.config.AutoSidedConfig;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin
+{
+    /**
+     * Allows items on the ground larger than 64 to merge into 1 stack.
+     */
+    @ModifyConstant(method = "merge(Lnet/minecraft/entity/item/ItemEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)V", constant = @Constant(intValue = 64))
+    private static int increaseStackLimit(int val){
+        return AutoSidedConfig.getMaxStackSize();
+    }
+}
