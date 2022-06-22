95c08c481a2820cd63979b022545858790c5feae
PORTB <alexander.pandaman@gmail.com>
2022-06-22 18:41:41 +1000

    Run code cleanup

--- src/main/java/portb/biggerstacks/mixin/vanilla/stacksize/SlotItemHandlerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/vanilla/stacksize/SlotItemHandlerMixin.java
@@ -1,13 +1,10 @@
 package portb.biggerstacks.mixin.vanilla.stacksize;
 
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.items.SlotItemHandler;
-import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageInventorySlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import portb.biggerstacks.Constants;
 import portb.biggerstacks.config.AutoSidedConfig;
 
 @Mixin(SlotItemHandler.class)
