09d439f51f6e1a0f7967ee1ab2d73a3bd8dcbca9
PORTB <PORTB@noreply.codeberg.org>
2022-06-23 20:19:07 +1000

    Add Terraqueous compat

---
+++ src/main/java/portb/biggerstacks/mixin/compat/sheiphiancore/InventoryInternalMixin.java
@@ -0,0 +1,23 @@
+package portb.biggerstacks.mixin.compat.sheiphiancore;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import portb.biggerstacks.config.AutoSidedConfig;
+import shetiphian.core.common.inventory.InventoryInternal;
+
+@Mixin(InventoryInternal.class)
+public abstract class InventoryInternalMixin
+{
+    @ModifyVariable(method = "<init>(Lnet/minecraft/tileentity/TileEntity;Ljava/lang/String;IILjava/lang/String;Lshetiphian/core/common/inventory/IContainerCallback;)V",
+                    at = @At("HEAD"),
+                    ordinal = 1,
+                    argsOnly = true)
+    private static int increaseStackLimit(int val)
+    {
+        if (val != 1)
+            return val * AutoSidedConfig.getMaxStackSize() / 64;
+        else
+            return val;
+    }
+}
