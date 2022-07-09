8026032b71a3c0e1d47dda9b55e639a10e78503e
PORTB <PORTB@noreply.codeberg.org>
2022-07-09 12:22:30 +1000

    Add missing remap=false

--- src/main/java/portb/biggerstacks/mixin/compat/sheiphiancore/InventoryInternalMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sheiphiancore/InventoryInternalMixin.java
@@ -3,7 +3,6 @@ package portb.biggerstacks.mixin.compat.sheiphiancore;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import portb.biggerstacks.config.AutoSidedConfig;
 import portb.biggerstacks.util.StackSizeHelper;
 import shetiphian.core.common.inventory.InventoryInternal;
 
@@ -13,7 +12,8 @@ public abstract class InventoryInternalMixin
     @ModifyVariable(method = "<init>(Lnet/minecraft/world/level/block/entity/BlockEntity;Ljava/lang/String;IILjava/lang/String;Lshetiphian/core/common/inventory/IContainerCallback;)V",
                     at = @At("HEAD"),
                     ordinal = 1,
-                    argsOnly = true)
+                    argsOnly = true,
+                    remap = false)
     private static int increaseStackLimit(int val)
     {
         return StackSizeHelper.scaleSlotLimit(val);
