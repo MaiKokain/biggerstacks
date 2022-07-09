8ff774fae62a035d3f89c2ea4b1d5db9c53c1f58
PORTB <PORTB@noreply.codeberg.org>
2022-07-09 20:40:28 +1000

    Fix sophisticated backpacks... AGAIN

--- src/main/java/portb/biggerstacks/mixin/compat/sophisticatedcore/BackpackContainerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedcore/BackpackContainerMixin.java
@@ -1,10 +1,14 @@
 package portb.biggerstacks.mixin.compat.sophisticatedcore;
 
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.Slot;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import portb.biggerstacks.config.AutoSidedConfig;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import portb.biggerstacks.util.StackSizeHelper;
 
 @Mixin(StorageContainerMenuBase.class)
 public abstract class BackpackContainerMixin
@@ -12,6 +16,18 @@ public abstract class BackpackContainerMixin
     @ModifyConstant(method = "calculateMaxCountForStack", constant = @Constant(intValue = 64), remap = false)
     private static int increaseStackLimit(int val)
     {
-        return AutoSidedConfig.getMaxStackSize();
+        return StackSizeHelper.getNewStackSize();
+    }
+    
+    @Redirect(method = "mergeItemStack(Lnet/minecraft/world/item/ItemStack;IIZZZ)Z",
+              at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;getMaxStackSize()I"))
+    private int avoidDoubleIncreasingStackSize(Slot slot)
+    {
+        if (slot.container.getClass() == Inventory.class)
+            //If the slot is an inventory slot, calling slot.getMaxStackSize() will end up increasing the stack size twice from 2 different mixins.
+            //to avoid this, just call StackSizeHelper directly. Yes it's hacky but this whole stupid mod is just a massive crappy hack
+            return StackSizeHelper.getNewStackSize();
+        else
+            return slot.getMaxStackSize();
     }
 }
--- src/main/java/portb/biggerstacks/mixin/compat/sophisticatedcore/InventoryHandlerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedcore/InventoryHandlerMixin.java
@@ -3,24 +3,29 @@ package portb.biggerstacks.mixin.compat.sophisticatedcore;
 import net.p3pp3rf1y.sophisticatedcore.inventory.InventoryHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import portb.biggerstacks.config.AutoSidedConfig;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import portb.biggerstacks.util.StackSizeHelper;
 
 @Mixin(InventoryHandler.class)
 public class InventoryHandlerMixin
 {
-    @ModifyVariable(method = "setSlotLimit", at = @At("HEAD"), ordinal = 0, remap = false, argsOnly = true)
+    //@ModifyVariable(method = "setSlotLimit", at = @At("HEAD"), ordinal = 0, remap = false, argsOnly = true)
     private int scaleSlotLimit(int slotLimit)
     {
         //Avoid potentially returning 0 with math.max
-        return Math.max(1, slotLimit * AutoSidedConfig.getMaxStackSize() / 64);
+        return StackSizeHelper.scaleSlotLimit(slotLimit);
     }
-
-    @ModifyConstant(method = "setSlotLimit", constant = @Constant(intValue = 64), remap = false)
+    
+    //@ModifyConstant(method = "setSlotLimit", constant = @Constant(intValue = 64), remap = false)
     private int increaseStackLimit(int val)
     {
-        return AutoSidedConfig.getMaxStackSize();
+        return StackSizeHelper.getNewStackSize();
+    }
+    
+    @Inject(method = "getStackLimit", at = @At("RETURN"), cancellable = true, remap = false)
+    private void increaseStackLimit(CallbackInfoReturnable<Integer> cir)
+    {
+        StackSizeHelper.scaleSlotLimit(cir);
     }
 }
