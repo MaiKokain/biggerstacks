dcde34fe5229e7902c30776788250a6a01e5a1ab
PORTB <PORTB@noreply.codeberg.org>
2022-07-10 16:13:49 +1000

    Fix sophisticated backpacks (again)

--- src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/InventoryHandlerMixin.java
+++ src/main/java/portb/biggerstacks/mixin/compat/sophisticatedbackpacks/InventoryHandlerMixin.java
@@ -3,18 +3,14 @@ package portb.biggerstacks.mixin.compat.sophisticatedbackpacks;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackInventoryHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import portb.biggerstacks.config.AutoSidedConfig;
 import portb.biggerstacks.util.StackSizeHelper;
 
 @Mixin(BackpackInventoryHandler.class)
 public class InventoryHandlerMixin
 {
-    @Shadow private int slotLimit;
-    
     @ModifyVariable(method = "setSlotLimit", at = @At("HEAD"), ordinal = 0, remap = false, argsOnly = true)
     private int scaleSlotLimit(int slotLimit)
     {
