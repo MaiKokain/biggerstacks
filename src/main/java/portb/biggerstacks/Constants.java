4220a2f5058a8d05c649468f39ecc59d6e8ecc17
PORTB <PORTB@noreply.codeberg.org>
2022-12-31 12:55:50 +1100

    Update Constants.java and lang file
    Totally not doing everything at once and messing up all my commits, why would you think that??

--- src/main/java/portb/biggerstacks/Constants.java
+++ src/main/java/portb/biggerstacks/Constants.java
@@ -1,11 +1,25 @@
 package portb.biggerstacks;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+import java.text.DecimalFormat;
+
 public class Constants
 {
     public static final int ONE_BILLION  = 1_000_000_000;
     public static final int ONE_MILLION  = 1_000_000;
     public static final int ONE_THOUSAND = 1_000;
-
-    public static final String MOD_ID = "biggerstacks";
+    
+    public static final String MOD_ID            = "biggerstacks";
     public static final String RULESET_FILE_NAME = "biggerstacks-rules.xml";
+    
+    public static final Path RULESET_FILE = FMLPaths.CONFIGDIR.get().resolve(Constants.RULESET_FILE_NAME);
+    
+    public static final DecimalFormat TOOLTIP_NUMBER_FORMAT = new DecimalFormat("###,###,###,###,###,###");
+    
+    public static final ResourceLocation CONFIG_GUI_BG = new ResourceLocation(Constants.MOD_ID,
+                                                                              "textures/gui/config_background.png"
+    );
 }
