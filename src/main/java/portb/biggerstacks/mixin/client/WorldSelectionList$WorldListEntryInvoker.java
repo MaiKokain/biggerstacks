45434ef5fb3352c6e47aed3c72806e78e5d8aca3
PORTB <PORTB@noreply.codeberg.org>
2023-01-01 18:18:32 +1100

    Fix warning screen not showing on new world creation

---
+++ src/main/java/portb/biggerstacks/mixin/client/WorldSelectionList$WorldListEntryInvoker.java
@@ -0,0 +1,26 @@
+package portb.biggerstacks.mixin.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
+import net.minecraft.client.gui.screens.worldselection.WorldSelectionList;
+import net.minecraft.world.level.storage.LevelSummary;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(WorldSelectionList.WorldListEntry.class)
+public interface WorldSelectionList$WorldListEntryInvoker
+{
+    @Invoker("queueLoadScreen")
+    void invokeQueueLoadScreen();
+    
+    @Accessor("minecraft")
+    Minecraft getMinecraft();
+    
+    @Accessor("summary")
+    LevelSummary getSummary();
+    
+    @Accessor("screen")
+    SelectWorldScreen accessScreen();
+}
+
