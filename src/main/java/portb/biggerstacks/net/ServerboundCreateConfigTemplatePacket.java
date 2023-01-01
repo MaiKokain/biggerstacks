6c641f8d5bdb5651f2046b7f298e823739e31f4a
PORTB <PORTB@noreply.codeberg.org>
2023-01-01 16:26:44 +1100

    IC2 doesn't exist for 1.18

--- src/main/java/portb/biggerstacks/net/ServerboundCreateConfigTemplatePacket.java
+++ src/main/java/portb/biggerstacks/net/ServerboundCreateConfigTemplatePacket.java
@@ -1,18 +1,15 @@
 package portb.biggerstacks.net;
 
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.network.NetworkEvent;
 import portb.biggerstacks.Constants;
 import portb.biggerstacks.config.ClientConfig;
 import portb.configlib.template.ConfigTemplate;
-import portb.configlib.xml.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class ServerboundCreateConfigTemplatePacket extends GenericTemplateOptionsPacket
@@ -34,22 +31,6 @@ public class ServerboundCreateConfigTemplatePacket extends GenericTemplateOption
             return;
         
         ConfigTemplate template = ConfigTemplate.generateTemplate(serverboundCreateConfigTemplatePacket);
-        
-        if (ModList.get().isLoaded("ic2"))
-        {
-            //limit ic2 upgrades to 64 (if it is installed), or issues might occur with putting too many into machines
-            template.getRules().add(new Rule(Arrays.asList(
-                                            new Condition(Property.MOD_ID, Operator.EQUALS, "ic2"),
-                                            new OrBlock(
-                                                    Arrays.asList(
-                                                            new Condition(Property.ID, Operator.STRING_STARTS_WITH, "ic2:upgrade"),
-                                                            new Condition(Property.ID, Operator.STRING_ENDS_WITH, "pad_upgrade"),
-                                                            new Condition(Property.ID, Operator.STRING_ENDS_WITH, "upgrade_kit")
-                                                    )
-                                            )
-                                    ), 64)
-            );
-        }
         
         try
         {
