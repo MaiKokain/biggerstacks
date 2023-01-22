10973b5f71851a3eb6e6580eefd2ecc130801dfc
PORTB <PORTB@noreply.codeberg.org>
2023-01-22 16:52:55 +1100

    Clean up stack size warning logic

--- src/main/java/portb/biggerstacks/mixin/client/WorldSelectionListMixin.java
+++ src/main/java/portb/biggerstacks/mixin/client/WorldSelectionListMixin.java
@@ -30,15 +30,15 @@ public class WorldSelectionListMixin
     {
         if (!ClientConfig.stfuWarning.get())
         {
-            int max = ConfigLib.readRuleset(FMLPaths.CONFIGDIR.get().resolve(Constants.RULESET_FILE_NAME)).getMaxStacksize();
+            int configMax = ServerConfig.LOCAL_INSTANCE.globalMaxStackSize.get();
+            int actualMax = ConfigLib.readRuleset(FMLPaths.CONFIGDIR.get().resolve(Constants.RULESET_FILE_NAME)).getMaxStacksize();
     
             //check if user may have been relying on value cap before update
-            if (ServerConfig.LOCAL_INSTANCE.globalMaxStackSize.get() != 1 &&
-                        max > ServerConfig.LOCAL_INSTANCE.globalMaxStackSize.get())
+            if (configMax != 1 && actualMax > configMax)
             {
                 ci.cancel();
         
-                HighStackSizeWarning.createWarningScreen(max, () -> {
+                HighStackSizeWarning.createWarningScreen(actualMax, () -> {
                     WorldSelectionList$WorldListEntryInvoker invoker = ((WorldSelectionList$WorldListEntryInvoker) this);
                     invoker.invokeQueueLoadScreen();
                     invoker.getMinecraft().loadLevel(invoker.getSummary().getLevelId());
