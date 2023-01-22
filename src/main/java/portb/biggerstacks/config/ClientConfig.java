e31b4a3dbc42443d30c6185642823dd70599fecb
PORTB <PORTB@noreply.codeberg.org>
2023-01-22 13:01:16 +1100

    Allow tooltip number colour to be configured

--- src/main/java/portb/biggerstacks/config/ClientConfig.java
+++ src/main/java/portb/biggerstacks/config/ClientConfig.java
@@ -7,20 +7,38 @@
 
 package portb.biggerstacks.config;
 
+import net.minecraft.ChatFormatting;
 import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Config used for client-side only things, such as how stack numbers are rendered.
  */
 public class ClientConfig
 {
-    public static final  ForgeConfigSpec              SPEC;
-    public static final  ForgeConfigSpec.BooleanValue enableNumberShortening;
-    public static final  ForgeConfigSpec.BooleanValue stfuWarning;
-    private static final ForgeConfigSpec.Builder      builder = new ForgeConfigSpec.Builder();
+    public static final  ForgeConfigSpec                     SPEC;
+    public static final  ForgeConfigSpec.BooleanValue        enableNumberShortening;
+    public static final  ForgeConfigSpec.BooleanValue        stfuWarning;
+    private static final Map<String, ChatFormatting> NUMBER_FORMATTING_COLOURS = new HashMap<>();
+    private static final ForgeConfigSpec.ConfigValue<String> numberColour;
+    
+    private static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
     
     static
     {
+        EnumSet.complementOf(EnumSet.of(
+                ChatFormatting.STRIKETHROUGH,
+                ChatFormatting.BOLD,
+                ChatFormatting.RESET,
+                ChatFormatting.UNDERLINE,
+                ChatFormatting.ITALIC,
+                ChatFormatting.OBFUSCATED
+        )).forEach(chatFormatting -> NUMBER_FORMATTING_COLOURS.put(chatFormatting.getName(), chatFormatting));
+        
         builder.comment("Client configs");
         enableNumberShortening = builder.comment("Enable number shortening. E.g. 1000000 becomes 1M.")
                                         .define("Enable number shortening", true);
@@ -31,6 +49,20 @@ public class ClientConfig
                 false
         );
         
+        numberColour = builder.comment("The colour of the exact count tooltip shown on items.",
+                                       "Available colours (case insensitive):",
+                                       String.join(",\n", NUMBER_FORMATTING_COLOURS.keySet().stream().sorted().toList())
+        ).define("Exact count number colour", ChatFormatting.DARK_AQUA.getName(),
+                 value -> NUMBER_FORMATTING_COLOURS.containsKey(Objects.requireNonNullElse((String) value,
+                                                                                           ""
+                 ).toLowerCase())
+        );
+        
         SPEC = builder.build();
+    }
+    
+    public static ChatFormatting getNumberColour()
+    {
+        return NUMBER_FORMATTING_COLOURS.get(numberColour.get().toLowerCase());
     }
 }
