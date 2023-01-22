70cab5b02e55f90197551af4391396a091a6fe67
PORTB <PORTB@noreply.codeberg.org>
2023-01-22 12:37:26 +1100

    Allow tooltip number colour to be configured

--- src/main/java/portb/biggerstacks/config/ClientConfig.java
+++ src/main/java/portb/biggerstacks/config/ClientConfig.java
@@ -1,21 +1,49 @@
+/*
+ * Copyright (c) PORTB 2023
+ *
+ * Licensed under GNU LGPL v3
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
 package portb.biggerstacks.config;
 
+import net.minecraft.ChatFormatting;
 import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Config used for client-side only things, such as how stack numbers are rendered.
  */
 public class ClientConfig
 {
-    public static final ForgeConfigSpec              SPEC;
-    public static final ForgeConfigSpec.BooleanValue enableNumberShortening;
-    public static final ForgeConfigSpec.BooleanValue stfuWarning;
+    private static final Map<String, ChatFormatting> NUMBER_FORMATTING_COLOURS = new HashMap<>();
+    
+    public static final  ForgeConfigSpec                     SPEC;
+    public static final  ForgeConfigSpec.BooleanValue        enableNumberShortening;
+    public static final  ForgeConfigSpec.BooleanValue        stfuWarning;
+    private static final ForgeConfigSpec.ConfigValue<String> numberColour;
     
     private static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
     
+    public static ChatFormatting getNumberColour()
+    {
+        return NUMBER_FORMATTING_COLOURS.get(numberColour.get().toLowerCase());
+    }
     
     static
     {
+        EnumSet.complementOf(EnumSet.of(
+                ChatFormatting.STRIKETHROUGH,
+                ChatFormatting.BOLD,
+                ChatFormatting.RESET,
+                ChatFormatting.UNDERLINE,
+                ChatFormatting.ITALIC,
+                ChatFormatting.OBFUSCATED
+        )).forEach(chatFormatting -> NUMBER_FORMATTING_COLOURS.put(chatFormatting.getName(), chatFormatting));
+        
         builder.comment("Client configs");
         
         enableNumberShortening = builder.comment("Enable number shortening. E.g. 1000000 becomes 1M.")
@@ -25,6 +53,13 @@ public class ClientConfig
                 "Disables warning for stack size that is potentially wrong due to behaviour changes.").define(
                 "STFU Warning",
                 false
+        );
+        
+        numberColour = builder.comment("The colour of the exact number count tooltip shown on items.",
+                                       "Available colours (case insensitive):",
+                                       String.join(",\n", NUMBER_FORMATTING_COLOURS.keySet().stream().sorted().toList())
+        ).define("Exact count number colour", ChatFormatting.DARK_AQUA.getName(),
+                 value -> NUMBER_FORMATTING_COLOURS.containsKey(((String) value).toLowerCase())
         );
         
         SPEC = builder.build();
--- src/main/java/portb/biggerstacks/event/ClientEvents.java
+++ src/main/java/portb/biggerstacks/event/ClientEvents.java
@@ -1,24 +1,24 @@
+/*
+ * Copyright (c) PORTB 2023
+ *
+ * Licensed under GNU LGPL v3
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
 package portb.biggerstacks.event;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import portb.biggerstacks.Constants;
 import portb.biggerstacks.config.ClientConfig;
 import portb.biggerstacks.config.StackSizeRules;
-import portb.biggerstacks.util.ConfigCommand;
-
-import java.nio.file.Files;
 
 import static portb.biggerstacks.Constants.TOOLTIP_NUMBER_FORMAT;
 
@@ -43,7 +43,7 @@ public class ClientEvents
                  .add(1,
                       Component.translatable("biggerstacks.exact.count",
                                              Component.literal(TOOLTIP_NUMBER_FORMAT.format(stack.getCount()))
-                                                      .withStyle(ChatFormatting.DARK_AQUA)
+                                                      .withStyle(ClientConfig.getNumberColour())
                                )
                                .withStyle(ChatFormatting.GRAY)
                  );
