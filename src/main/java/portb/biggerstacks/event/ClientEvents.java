7d6fd66585f5648bd92065bc3a4d5ab064c4aa76
PORTB <PORTB@noreply.codeberg.org>
2023-01-23 11:23:47 +1100

    Allow tooltip number colour to be configured

--- src/main/java/portb/biggerstacks/event/ClientEvents.java
+++ src/main/java/portb/biggerstacks/event/ClientEvents.java
@@ -8,24 +8,17 @@
 package portb.biggerstacks.event;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.Style;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import portb.biggerstacks.Constants;
 import portb.biggerstacks.config.ClientConfig;
 import portb.biggerstacks.config.StackSizeRules;
-import portb.biggerstacks.util.ConfigCommand;
-
-import java.nio.file.Files;
 
 import static portb.biggerstacks.Constants.TOOLTIP_NUMBER_FORMAT;
 
@@ -50,7 +43,7 @@ public class ClientEvents
                  .add(1,
                       Component.translatable("biggerstacks.exact.count",
                                              Component.literal(TOOLTIP_NUMBER_FORMAT.format(stack.getCount()))
-                                                      .withStyle(ChatFormatting.DARK_AQUA)
+                                                      .withStyle(ClientConfig.getNumberColour())
                                )
                                .withStyle(ChatFormatting.GRAY)
                  );
