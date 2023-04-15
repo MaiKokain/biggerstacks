1c03bbd333515d2cab6015195c699639270203b5
PORTB <PORTB@noreply.codeberg.org>
2023-04-15 11:51:13 +1000

    Change 'warning' message that hardly ever signaled that something was wrong to debug level

--- src/main/java/portb/biggerstacks/util/ItemStackSizeHelper.java
+++ src/main/java/portb/biggerstacks/util/ItemStackSizeHelper.java
@@ -45,7 +45,7 @@ public class ItemStackSizeHelper
         }
         else
         {
-            LOGGER.warn("Stack size ruleset is somehow null, using fallback logic. Called from " +
+            LOGGER.debug("Stack size ruleset is somehow null, using fallback logic. Called from " +
                                 CallingClassUtil.getCallerClassName());
             
             if (returnInfo.getReturnValue() > 1)
