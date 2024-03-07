b21275bcd8a8cb99b93985ed2ceb0d75d7257876
Yuuria <yuuria_sakura@proton.me>
2024-03-07 21:29:04 +0700

    Hopefully fix /submodules

--- apply-submodule-patch.sh
+++ apply-submodule-patch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-git am -3 --ignore-whitespace -k ./submodules/BiggerStacksTransformerLib.patch
-git am -3 --ignore-whitespace -k ./submodules/MinecraftSubmoduleDependencyPlugin.patch
+# git am -3 --ignore-whitespace -k ./submodules/BiggerStacksTransformerLib.patch
+# git am -3 --ignore-whitespace -k ./submodules/MinecraftSubmoduleDependencyPlugin.patch
+patch ./submodules/MinecraftSubmoduleDependencyPlugin/build.gradle ./submodules/module_dep/SubmoduleDependency.build.patch
+patch ./submodules/MinecraftSubmoduleDependencyPlugin/src/main/groovy/portb/submoduleplugin/Repo.groovy ./submodules/module_dep/SubmoduleDependency.repo.patch
+
+patch ./submodules/BiggerStacksTransformerLib/build.gradle ./submodules/TransformerLib.patch
